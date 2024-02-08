c754ce817216ca0edaa0358c71f500a3b0c5f517
urizennnnnn <igamerryt@gmail.com>
2024-02-08 18:20:56 +0100

    Visual Studio Code layout

--- main.go
+++ main.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
-	// label:=widget.NewLabel("Hello World!")
-
-	btn := widget.NewButton("Testing button", func() {
-		fmt.Println("Button working")
+	w := a.NewWindow("Script Pad")
+	w.Resize(fyne.NewSize(400, 400))
+	fileExplorer := widget.NewButton("File Explorer",func ()  {
+		fmt.Println("File Explorer Section")
 	})
-	w.SetContent(btn)
+	Textarea := canvas.NewText("Text Area",color.Black)
+	toolbar:=widget.NewButton("Toolbar",func() {
+		fmt.Print("Toolbar")
+	})
+
+	w.SetContent(
+		container.NewVBox(toolbar,
+		container.NewHSplit(
+			fileExplorer,Textarea,
+		),
+		),
+		
+	)
 	w.ShowAndRun()
 }
