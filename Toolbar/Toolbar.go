eb9387f62ff6f2260b9c1a0dd6fcbec978b58485
urizennnnnn <igamerryt@gmail.com>
2024-02-09 22:47:39 +0100

    adding menu tool bar

---
+++ Toolbar/Toolbar.go
@@ -0,0 +1,12 @@
+package Toolbar
+
+import "fyne.io/fyne/v2"
+
+var File = fyne.NewMenu("File", nil)
+var Edit = fyne.NewMenu("Edit", nil)
+var View = fyne.NewMenu("View", nil)
+var Selection = fyne.NewMenu("Selection", nil)
+var Go = fyne.NewMenu("Go", nil)
+var Run = fyne.NewMenu("Run", nil)
+var Terminal = fyne.NewMenu("Terminal", nil)
+var Help = fyne.NewMenu("Help", nil)
