f8647e99cdb75f40683d05948358520e7a78f4bc
urizennnnnn <igamerryt@gmail.com>
2024-02-09 18:15:38 +0100

    added file explorer section

---
+++ Explorer/Explorer.go
@@ -0,0 +1,3 @@
+package Explorer
+
+var FieldItem = map[int]string{1: "Explorer", 2: "Search", 3: "Git", 4: "Debug", 5: "Extensions"}
--- main.go
+++ main.go
@@ -1,45 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"image/color"
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
+    "fmt"
+    "fyne.io/fyne/v2"
+    "fyne.io/fyne/v2/app"
+    "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/widget"
+    "github.com/urizennnn/ScriptPad/Explorer"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Script Pad")
-	w.Resize(fyne.NewSize(400, 400))
-	icon,err:=fyne.LoadResourceFromPath("./images/chinmay-b-fd9mIBluHkA-unsplash.jpg")
-	if err != nil {
-		panic(err)
-	}
-	
-	a.SetIcon(icon)
-
-	setTheme := widget.NewButton("Dark Theme", func() {
-		a.Settings().SetTheme(theme.DarkTheme())
-	})
-
-	fileExplorer := widget.NewButton("File Explorer", func() {
-		fmt.Println("File Explorer Section")
-	})
-	Textarea := canvas.NewText("Text Area", color.Black)
-	toolbar := widget.NewButton("Toolbar", func() {
-		fmt.Print("Toolbar")
-	})
-
-	w.SetContent(
-		container.NewVBox(toolbar, setTheme,
-			container.NewHSplit(
-				fileExplorer, Textarea,
-			),
-		),
-	)
-	w.ShowAndRun()
+    a := app.New()
+    w := a.NewWindow("Script Pad")
+    w.Resize(fyne.NewSize(1200, 800))
+    icon, err := fyne.LoadResourceFromPath("C:/Users/Victor/Desktop/Urizen's Text  Editor/images/chinmay-b-fd9mIBluHkA-unsplash.jpg")
+    if err != nil {
+        fmt.Print(err)
+        a.Quit()
+        return
+    }
+    
+    a.SetIcon(icon)
+    
+    ExplorerSection := widget.NewList(
+        func() int {
+            return len(Explorer.FieldItem)
+        }, func() fyne.CanvasObject {
+            return widget.NewLabel("")
+        }, func(id widget.ListItemID, object fyne.CanvasObject) {
+            object.(*widget.Label).SetText(Explorer.FieldItem[id])
+        },
+    )
+    
+    // Set a max size for the split
+    maxSize := fyne.NewSize(8, 6)
+    split := container.NewHSplit(ExplorerSection, container.NewMax())
+    split.Resize(maxSize)
+    split.Offset = 0.
+    
+    w.SetContent(split)
+    w.ShowAndRun()
 }
